4d0694443f91121be09489eb8dcb0cef9abffdd9
Rafael dos Santos <rafaels1997@LIVE.COM>
2022-05-15 19:06:42 -0300

    Iniciada tela de configurações

---
+++ projetoMobile/app/src/main/java/br/univali/pdm/projetomobile/ui/activity/ConfiguracoesActivity.java
@@ -0,0 +1,16 @@
+package br.univali.pdm.projetomobile.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.univali.pdm.projetomobile.R;
+
+public class ConfiguracoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+    }
+}
