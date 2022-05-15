570eb9643c2358658df95e6f6119439a40729013
Rafael dos Santos <rafaels1997@LIVE.COM>
2022-05-14 22:31:54 -0300

    Criação de campos

--- app/src/main/java/rafael/treinamento/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/rafael/treinamento/ui/activity/ListaAlunosActivity.java
@@ -16,10 +16,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Rafael", "João", "José", "Maria", "Carlos"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
