9daffa809e23a4b978f2b4394b7c54f5303349bf
Rafael dos Santos <rafaels1997@LIVE.COM>
2022-05-15 19:04:26 -0300

    Adicionado menu com configurações e perfil

--- projetoMobile/app/src/main/java/br/univali/pdm/projetomobile/ui/activity/HomeActivity.java
+++ projetoMobile/app/src/main/java/br/univali/pdm/projetomobile/ui/activity/HomeActivity.java
@@ -1,54 +1,47 @@
 package br.univali.pdm.projetomobile.ui.activity;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import br.univali.pdm.projetomobile.databinding.ActivityHomeBinding;
-//import br.univali.pdm.projetomobile.ui.activity.databinding.ActivityHomeBinding;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import br.univali.pdm.projetomobile.R;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityHomeBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_home);
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_configuracoes, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_configuracoes:
+
+                Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show();
+                
+                break;
+                
+            case R.id.item_perfil:
+
+                Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show();
+                
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
